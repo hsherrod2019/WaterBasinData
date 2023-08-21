ee1ec9f9102a84ceea1ec8fac0168107427da057
hsherrod2019 <hsherrod2019@gmail.com>
2023-08-21 15:32:33 -0700

    Comparing with standard baseline

--- random_forest.R
+++ random_forest.R
@@ -46,9 +46,28 @@ predictions <- predict(rf_model, newdata = test_subset)
 cat("Predicted values:", predictions, "\n")
 cat("Actual values:", test_subset$mp_conc_ppm3, "\n")
 
+###
+
 # Evaluate the model (you can use various evaluation metrics)
 accuracy <- mean(predictions == test_subset$mp_conc_ppm3)
 
 # Calculate RMSE- regression task predicts continuous target variable, remove accuracy calculation
 rmse <- sqrt(mean((predictions - test_subset$mp_conc_ppm3)^2))
 cat("RMSE:", rmse, "\n")
+
+# Calculate baseline prediction (mean or median)
+baseline_prediction <- mean(train_data$mp_conc_ppm3)  # You can also use median if appropriate
+
+# Create a vector of baseline predictions for the test set
+baseline_predictions <- rep(baseline_prediction, nrow(test_data))
+
+# Calculate baseline RMSE
+baseline_rmse <- sqrt(mean((baseline_predictions - test_data$mp_conc_ppm3)^2))
+
+# Calculate your model's RMSE (you've already calculated this)
+model_rmse <- 1535.988  # Replace with your actual model's RMSE
+
+# Compare the two RMSE values
+cat("Baseline RMSE:", baseline_rmse, "\n")
+cat("Model RMSE:", model_rmse, "\n")
+
