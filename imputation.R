73303fb43f7392341a9d359f4be7c9d0dc671cb7
nickleong20 <nick@mooreplasticresearch.org>
2023-08-23 10:57:25 -0700

    create imputation code

---
+++ imputation.R
@@ -0,0 +1,22 @@
+# Assuming your data frame is named 'cleaned_data' and the columns for calculating fitted values are 'drnarea', 'precip', 'lc01dev_lc11dev', 'x50_percent_aep_flood', and 'bsldem30m'
+# Also assuming that you've loaded the 'NADA' package using library(NADA)
+
+library(dplyr)
+library(NADA)
+
+# Add a new column that checks for zeros
+cleaned_data <- cleaned_data %>%
+  mutate(censored = ifelse(standardized_data_in_ppm3 == 0, TRUE, FALSE))
+
+fit <- cenros(cleaned_data$standardized_data_in_ppm3, cleaned_data$censored)
+
+
+set.seed(211)
+
+# Impute zero values using the cenros function from NADA package
+fittedvalues <- sample(fit$modeled[fit$censored], length(fit$modeled[fit$censored]), replace = F)
+
+#Update the data frame with imputed values
+cleaned_data <- cleaned_data %>%
+  mutate(imputed_standardized_data = ifelse(censored, fittedvalues, standardized_data_in_ppm3))
+
